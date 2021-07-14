21acaf22d55c8b43acff505f4d44b288ff08cffc
Subha Das <rohan6056@hotmail.com>
2021-07-13 18:38:20 -0700

    Refactored customers, added additional fields to queries and mutations

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/nhost/stripe-graphql
 go 1.16
 
 require (
-	github.com/99designs/gqlgen v0.13.0 // indirect
-	github.com/stripe/stripe-go v70.15.0+incompatible
-	github.com/stripe/stripe-go/v72 v72.53.0 // indirect
-	github.com/vektah/gqlparser/v2 v2.1.0 // indirect
+	github.com/99designs/gqlgen v0.13.0
+	github.com/hasura/go-graphql-client v0.2.0
+	github.com/stripe/stripe-go/v72 v72.53.0
+	github.com/vektah/gqlparser/v2 v2.1.0
 )
--- graph/utils/customerConversion.go
+++ graph/utils/customerConversion.go
@@ -8,6 +8,7 @@ import (
 func ConvertCustomer(old_customer *stripe.Customer) *model.Customer {
 	currency := string(old_customer.Currency)
 	created := int(old_customer.Created)
+	address := ConvertAddress(&old_customer.Address)
 	var subscriptions []*model.StripeSubscription
 	if old_customer.Subscriptions != nil {
 		for _, sub := range old_customer.Subscriptions.Data {
@@ -18,7 +19,7 @@ func ConvertCustomer(old_customer *stripe.Customer) *model.Customer {
 		ID:           old_customer.ID,
 		Name:         &old_customer.Name,
 		Email:        &old_customer.Email,
-		Address:      &old_customer.Address.Line1,
+		Address:      address,
 		Phone:        &old_customer.Phone,
 		Currency:     &currency,
 		Created:      &created,
@@ -27,4 +28,17 @@ func ConvertCustomer(old_customer *stripe.Customer) *model.Customer {
 	}
 
 	return new_customer
+}
+
+func ConvertAddress(old_address *stripe.Address) *model.Address {
+	new_address := &model.Address{
+		City:       &old_address.City,
+		Country:    &old_address.Country,
+		PostalCode: &old_address.PostalCode,
+		State:      &old_address.State,
+		Line1:      &old_address.Line1,
+		Line2:      &old_address.Line2,
+	}
+
+	return new_address
 }
